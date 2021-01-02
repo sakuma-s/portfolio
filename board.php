8d65ddb0992ac6a7245244f8ca555e7137daf2f8
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-01-02 17:30:08 +0900

    データベース接続 HTML大枠実装

--- board.php
+++ board.php
@@ -1,9 +1,41 @@
 <?php
+//DB接続
+try {
+    $db = new PDO('mysql:dbname=board;host=localhost;port=8889;charset=utf8', 'root', 'root');
+} catch (PDOException $e) {
+    echo 'DB接続エラー: ' . $e->getMessage();
+}
 //投稿の入力
 echo 'name:';
 $name = trim(fgets(STDIN));
 echo 'text:';
 $text = trim(fgets(STDIN));
-//投稿一覧(表示数を10件程度にする)
-//投稿の返信
-//投稿の削除
+?>
+<!--投稿一覧(表示数を10件程度にする)-->
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>掲示板(仮)</title>
+</head>
+
+<body>
+    <h1>掲示板(仮)</h1>
+    <form action="create.php" method="POST">
+        <!--エラー文記入-->
+        <div>
+            <label for="name">名前</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div>
+            <label for="message">今の弱音をお書きください。多分誰かが励ましてくれます。</label>
+            <textarea type="text" name="message" row="10" cols="30"></textarea>
+        </div>
+    </form>
+</body>
+
+</html>
+<!--投稿の返信-->
+<!--投稿の削除-->
