b43b0a853a4f7f47bb143b47b63a5ce410da665d
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-01-04 07:20:38 +0900

    データ登録の編集

--- create.php
+++ create.php
@@ -1,15 +1,13 @@
 <?php
 require('dbconnect.php');
 //データ登録
-if (!empty($_POST)) {
-    if ($_POST['message'] !== '') {
-        $message = $db->prepare('INSERT INTO posts SET name=?, message=?, created=NOW()');
-        $message->execute(array($_POST['name'], $_POST['message']));
-        // ('Location: board.php');
-        // exit();
-    } else {
-        echo '何か入力してください';
-    }
+if (!empty($_POST['submit'])) {
+    $message = $db->prepare('INSERT INTO posts SET name=?, message=?, created=NOW()');
+    $message->execute(array($_POST['name'], $_POST['message']));
+    // ('Location: board.php');
+    // exit();
+} else {
+    echo '未入力部分があります。入力してください。';
 }
 //データ取得
 $records = $db->query('SELECT * FROM posts');
