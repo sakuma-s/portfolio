d3b064c6da429af17ec3dfef31eb1651fe8280ad
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-01-03 17:15:13 +0900

    データ登録・取得

--- create.php
+++ create.php
@@ -1,22 +1,20 @@
 <?php
 require('dbconnect.php');
-$statement = $db->prepare('INSERT INTO posts SET name=?, message=?, created=NOW()');
-$statement->execute(array($_POST['name'], $_POST['message']));
-echo 'メッセージが登録されました';
-
-// if (!empty($_POST)) {
-//     if($_POST['name'] !== '') {
-//         $name = $db->prepare('INSERT INTO posts SET name=?, message=?, created=NOW()');
-//         $message->execute(array(
-//             $
-//         ))
-//     }
-// }
-
+//データ登録
+if (!empty($_POST)) {
+    if ($_POST['message'] !== '') {
+        $message = $db->prepare('INSERT INTO posts SET name=?, message=?, created=NOW()');
+        $message->execute(array($_POST['name'], $_POST['message']));
+        // ('Location: board.php');
+        // exit();
+    } else {
+        echo '何か入力してください';
+    }
+}
 //データ取得
-// $records = $db->query('SELECT * FROM posts');
-// while ($record = $records->fetch()) {
-//     echo ($record['id'] . PHP_EOL);
-//     echo ($record['name'] . PHP_EOL);
-//     echo ($record['message'] . PHP_EOL);
-// }
+$records = $db->query('SELECT * FROM posts');
+while ($record = $records->fetch()) {
+    echo ($record['id'] . PHP_EOL);
+    echo ($record['name'] . PHP_EOL);
+    echo ($record['message'] . PHP_EOL);
+}
