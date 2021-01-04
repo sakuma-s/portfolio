984589b6a2db16e640fb166c10709687d6868f91
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-01-04 16:25:52 +0900

    バリデーション処理

--- create.php
+++ create.php
@@ -2,12 +2,25 @@
 require('dbconnect.php');
 //データ登録
 $statement = $db->prepare('INSERT INTO posts SET name=?, message=?, created=NOW()');
-$statement->execute(array($_POST['name'], $_POST['message']));
-// ('Location: board.php');
-// exit();
+$result = $statement->execute(array($_POST['name'], $_POST['message']));
+
+//バリデーション処理
+$errors_message = [];
+if (empty($_POST['name'])) {
+    $errors_message[] = 'ニックネームを入力してください';
+}
+if (empty($_POST['message'])) {
+    $errors_message[] = '投稿が未入力です';
+}
+
 $list = []; //データベースの取り出し
 //データ取得
 $sql = $db->query('SELECT * FROM posts');
 while ($posts = $sql->fetch()) {
     $list[] = $posts;
+}
+//二重登録防止
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    header("Location: board.php");
+    exit;
 }
