6cfb01964edc2899819fe7a4b9680879e99ef0c8
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-01-06 18:10:21 +0900

    関数追加

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,11 @@
 <?php
 //DB接続
-try {
-    $db = new PDO('mysql:dbname=board;host=localhost;port=8889;charset=utf8', 'root', 'root');
-} catch (PDOException $e) {
-    echo 'DB接続エラー: ' . $e->getMessage();
+function dbConnect()
+{
+    try {
+        $db = new PDO('mysql:dbname=board;host=localhost;port=8889;charset=utf8', 'root', 'root');
+    } catch (PDOException $e) {
+        echo 'DB接続エラー: ' . $e->getMessage();
+    }
+    return $db;
 }
