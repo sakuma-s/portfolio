2b24ae8e810fd5c36526a3b71246f521fdde8f96
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-01-07 17:58:12 +0900

    前回設定していた関数名をより短縮

--- escape.php
+++ escape.php
@@ -3,3 +3,5 @@ function h($string)
 {
     return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
 }
+//<?php echo htmlspecialchars($value['name'], ENT_QUOTES, "UTF-8");
+//<?php echo htmlspecialchars($value['message'], ENT_QUOTES, "UTF-8");
