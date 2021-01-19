5d77ae4fdcfcc40338e5ae1fcfb14fb82255b89e
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-01-20 05:58:36 +0900

    ページネーションの実装

--- board.php
+++ board.php
@@ -3,12 +3,19 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $errors = [];
+$now = [];
 require_once('dbconnect.php');
 require_once('create.php');
 $db = dbConnect();
 createBoard($db);
 deleteBoard($db, $id);
-$list = listBoard($db);
+list($page, $list) = pagiNation($db);
+
+// $allList = pagiNation($db);
+// $list = listBoard($db);
+// var_dump($allList);
+// $list = listBoard($db);
+//var_dump(h($list)); alertが出てしまうので注意！hを追加したから大丈夫かもしれない
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -46,6 +53,15 @@ $list = listBoard($db);
             <div><?php echo $value['created']; ?></div>
         <?php endforeach; ?>
     </main>
+    <div>
+        <?php for ($n = 1; $n <= $page; $n++) : ?>
+            <?php if ($n === $now) : ?>
+                <?php echo "<span style='padding: 5px;'>$now</span>"; ?>
+            <?php else : ?>
+                <?php echo "<a href='?page_id=$n' style='padding: 5px;'>$n</a>"; ?>
+            <?php endif; ?>
+        <?php endfor; ?>
+    </div>
 </body>
 
 </html>
--- create.php
+++ create.php
@@ -22,23 +22,45 @@ function validate($board)
     }
     return $errors;
 }
-function listBoard($db)
-{
-    //データベースの取り出し
-    $list = [];
-    //データ取得
-    $sql = $db->query('SELECT * FROM posts ORDER BY id DESC');
-    while ($posts = $sql->fetch(PDO::FETCH_ASSOC)) {
-        $list[] = $posts;
-    }
-    return $list;
-}
+// function listBoard($db)
+// {
+//     //データベースの取り出し
+//     $list = [];
+//     //データ取得
+//     $sql = $db->query('SELECT * FROM posts ORDER BY id DESC');
+//     while ($posts = $sql->fetch(PDO::FETCH_ASSOC)) {
+//         $list[] = $posts;
+//     }
+//     return $list;
+// }
 //ページネーションの作成
-function pagiNation($db, $count)
+function pagiNation($db)
 {
     define('max_view', 10);
     $count = $db->prepare('SELECT COUNT(*) AS count FROM posts');
     $count->execute();
+    $total_count = $count->fetch(PDO::FETCH_ASSOC);
+    $page = ceil($total_count['count'] / max_view);
+    //ページ番号の取得
+    if (!isset($_GET['page_id'])) {
+        $now = 1;
+    } else {
+        $now = $_GET['page_id'];
+    }
+    //表示する記事の取得
+    $select = $db->prepare("SELECT * FROM posts ORDER BY id DESC LIMIT :start,:max ");
+    //1ページ目の処理
+    if ($now === 1) {
+        $select->bindValue(":start", $now - 1, PDO::PARAM_INT);
+        $select->bindValue(":max", max_view, PDO::PARAM_INT);
+    } else {
+        $select->bindValue(":start", ($now - 1) * max_view, PDO::PARAM_INT);
+        $select->bindValue(":max", max_view, PDO::PARAM_INT);
+    }
+    $select->execute();
+    $list = $select->fetchAll(PDO::FETCH_ASSOC);
+
+    return [$page, $list];
 }
 //データの削除
 function deleteBoard($db, $id)
