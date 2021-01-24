5573dbefa4b5f9d4dadadf65e3edf2ef79bbcb5e
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-01-24 11:14:51 +0900

    例外処理の設定

--- dbconnect.php
+++ dbconnect.php
@@ -4,8 +4,10 @@ function dbConnect()
 {
     try {
         $db = new PDO('mysql:dbname=board;host=localhost;port=8889;charset=utf8', 'root', 'root');
-    } catch (PDOException $e) {
-        echo 'DB接続エラー: ' . $e->getMessage();
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    } catch (PDOException $Exception) {
+        die('DB接続エラー: ' . $Exception->getMessage());
     }
     return $db;
 }
