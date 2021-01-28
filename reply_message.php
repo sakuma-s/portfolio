be7faabb87ea7faeacd5f79be5c72a4be009b0f2
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-01-28 09:55:20 +0900

    デザイン変更・不要なコメント削除

--- reply_message.php
+++ reply_message.php
@@ -3,7 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require 'dbconnect.php';
-// $_POST['reply_message'] = filter_input(INPUT_POST, 'reply_message');
 //投稿へのコメント
 function commentBoard($db)
 {
@@ -24,16 +23,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>掲示板(仮)</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>励まし掲示板(仮)</title>
 </head>
 
 <body>
-    <h1>掲示板(仮)</h1>
-    <a href="board.php">一覧画面に戻る</a>
-    <form action="" method="POST">
-        <label for="reply_message">励ましの言葉をお願いいたします</label>
-        <textarea type="text" name="reply_message" id="reply_message" placeholder="140字までになります" maxlength="140" rows="6" cols="50"><?php echo $_REQUEST['nickname']; ?>さんへ&nbsp;</textarea>
-        <button type="submit">投稿</button>
+    <div class="container">
+        <h1 class="mt-3 mb-5"><a class="text-body text-decoration-none" href="board.php">励まし掲示板(仮)</h1>
+        <form action="" method="POST">
+            <div class="mb-2">
+                <label for="reply_message">励ましの言葉をお願いいたします</label>
+            </div>
+            <div class="mb-2">
+                <textarea type="text" class="form-control" name="reply_message" id="reply_message" placeholder="140字までになります" maxlength="140" rows="6" cols="50"><?php echo $_REQUEST['nickname']; ?>さんへ&nbsp;</textarea>
+            </div>
+
+            <button type="submit" class="btn btn-secondary">投稿</button>
+            <a class="btn btn-primary" href="board.php" role="button">一覧へ</a>
+    </div>
     </form>
 </body>
 
