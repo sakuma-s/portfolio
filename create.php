6d8f2c1c568d1650dea836db6b6030f9822f122b
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-01-29 18:17:09 +0900

    エラー処理を追加

--- create.php
+++ create.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require 'escape.php';
 $_GET['id'] = filter_input(INPUT_GET, 'id');
 $id = $_GET['id'];
