bb44d9d4dbd48657be1c099e3c7efcf63aa647e5
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-01-29 18:12:09 +0900

    バリデーション処理の追加とコメント先の名称変更

--- reply_message.php
+++ reply_message.php
@@ -2,19 +2,34 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require 'dbconnect.php';
+require 'connect.php';
+$errors = [];
 //投稿へのコメント
-function commentBoard($db)
+function commentBoard($db, $board)
 {
-    if (isset($_POST['reply_message'])) {
+    if (!empty($board['reply_message']) && (iconv_strlen($board['reply_message']) >= 7)) {
         $statement = $db->prepare('INSERT INTO posts SET reply_message=?, created=NOW()');
-        $statement->execute(array($_POST['reply_message']));
+        $result = $statement->execute(array($board['reply_message']));
     }
 }
-$db = dbconnect();
-commentBoard($db);
+function validate($board)
+{
+    $errors = [];
+    if (iconv_strlen($board['reply_message']) < 7) {
+        $errors['reply_message'] = 'コメントを入力してください';
+    }
+    return $errors;
+}
+$db = connect();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo '励ましました！一覧でご確認くださいませ!';
+    $board = [
+        'reply_message' => $_POST['reply_message']
+    ];
+    $errors = validate($board);
+    if (!count($errors)) {
+        commentBoard($db, $board);
+        echo '励ましました！一覧でご確認くださいませ!';
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -29,19 +44,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <body>
     <div class="container">
-        <h1 class="mt-3 mb-5"><a class="text-body text-decoration-none" href="board.php">励まし掲示板(仮)</h1>
+        <h1 class="mt-3 mb-5"><a class="text-body text-decoration-none" href="board.php">励まし掲示板(仮)</a></h1>
         <form action="" method="POST">
+            <?php if (count($errors) > 0) : ?>
+                <?php foreach ($errors as $error) : ?>
+                    <li><?php echo $error; ?></li>
+                <?php endforeach; ?>
+            <?php endif; ?>
             <div class="mb-2">
                 <label for="reply_message">励ましの言葉をお願いいたします</label>
             </div>
             <div class="mb-2">
-                <textarea type="text" class="form-control" name="reply_message" id="reply_message" placeholder="140字までになります" maxlength="140" rows="6" cols="50"><?php echo $_REQUEST['nickname']; ?>さんへ&nbsp;</textarea>
+                <textarea type="text" class="form-control" name="reply_message" id="reply_message" placeholder="140字までになります" maxlength="140" rows="6" cols="50"><?php echo $_REQUEST['id']; ?>&nbsp;&nbsp;</textarea>
             </div>
-
             <button type="submit" class="btn btn-secondary">投稿</button>
             <a class="btn btn-primary" href="board.php" role="button">一覧へ</a>
+        </form>
     </div>
-    </form>
 </body>
 
 </html>
