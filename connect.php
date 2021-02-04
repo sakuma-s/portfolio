4940cd0a9071f261b6c8082a1bf394c38737c89c
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-02-04 15:27:05 +0900

    データ・ベース接続変更2

--- connect.php
+++ connect.php
@@ -2,14 +2,13 @@
 //require __DIR__ . '/vendor/autoload.php';
 function connect()
 {
+    $db = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $db['DB_DATABASE'] = ltrim($db['path'], '/');
+    $dbHost = $db['DB_HOST'];
+    $dbUsername = $db['DB_USERNAME'];
+    $dbPassword = $db['DB_PASSWORD'];
+    $dbDatabase = substr($db['DB_DATABASE'], 1);
     try {
-        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-        $dbHost = $url['DB_HOST'];
-        $dbUsername = $url['DB_USERNAME'];
-        $dbPassword = $url['DB_PASSWORD'];
-        $dbDatabase = substr($url['DB_DATABASE'], 1);
-
         $db = new PDO("mysql:dbname={$dbDatabase}, host={$dbHost}, charset=utf8, {$dbUsername}, {$dbPassword}");
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
