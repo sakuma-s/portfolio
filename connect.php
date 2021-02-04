5b676ed0bace5ae21d6422fcc23fff978bd6e0cb
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-02-04 17:51:59 +0900

    データ・ベース接続変更4

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 function connect()
 {
     $db = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $db['dbname'] = ltrim($db['path'], '/');
+    $db['dbname'] = ltrim($db['path'], '/'); //pathのみ取り出す
     $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
     try {
         $db = new PDO($dsn, $db['user'], $db['pass']);
