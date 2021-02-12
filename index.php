6e48748aa1b720dc960182aa49a75718a6ffb9f5
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-02-12 14:25:24 +0900

    Revert "timezoneの確認のコードを取り消す"
    This reverts commit 92af2c8b5578a56375cea505397c2ba532192028.

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-var_dump(date_default_timezone_get());
 $errors = [];
 $board = [];
 require_once('connect.php');
