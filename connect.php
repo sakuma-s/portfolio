0f3c0384c37af7592f669391620579f0a0b8b463
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-02-16 08:14:16 +0900

    接続時にタイムゾーンを指定

--- connect.php
+++ connect.php
@@ -4,8 +4,9 @@ function connect()
     $db = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $db['dbname'] = ltrim($db['path'], '/'); //pathのみ取り出す
     $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+    $driver_options = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone='+09:00'"];
     try {
-        $db = new PDO($dsn, $db['user'], $db['pass']);
+        $db = new PDO($dsn, $db['user'], $db['pass'], $driver_options);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     } catch (PDOException $Exception) {
