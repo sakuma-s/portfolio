900de5cd0f3710190954d9b9a037f544fe44fe86
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-02-18 06:16:00 +0900

    [add]タイトル名変更

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ list($page, $maxPage, $list) = pagiNation($db);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <title>励まし掲示板(仮)</title>
+    <title>励まし掲示板</title>
 </head>
 
 <body>
