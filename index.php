69e7613a1149ed424f664b69eda2d70194faa5d9
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-02-27 15:56:11 +0900

    fontawesomeのCDNを追加

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ list($page, $maxPage, $list) = pagiNation($db);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <title>励まし掲示板</title>
 </head>
 
