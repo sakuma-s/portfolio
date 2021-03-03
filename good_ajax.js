f631b20383b87ae0d54393a0ebd28268ed9d0c31
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-03-03 16:15:37 +0900

    goodボタンにclassを追加し全てにインベントリスナーを適用させる

--- good_ajax.js
+++ good_ajax.js
@@ -1,20 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('btn').addEventListener('click', function () {
-    var result = document.getElementById('result');
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener('loadstart', function () {
-      result.textContent = '通信中...';
+  var good = document.getElementsByClassName('good');
+  console.dir(good);
+  for (var i = 0; i < good.length; i++) {
+    good[i].addEventListener('click', function () {
+      var result = document.getElementById('result');
+      var xhr = new XMLHttpRequest();
+      xhr.addEventListener('loadstart', function () {
+        result.textContent = '通信中...';
+      }, false);
+      xhr.addEventListener('load', function () {
+        result.textContent = xhr.responseText;
+      }, false);
+      xhr.addEventListener('error', function () {
+        result.textContent = 'サーバーエラーが発生しました。';
+      }, false);
+      xhr.open('POST', 'create.php', true);
+      xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+      xhr.send('value=' + encodeURIComponent(document.getElementById('btn').value));
+      //変数自体が存在するか確認
+      console.log(typeof value === 'undefined');
     }, false);
-    xhr.addEventListener('load', function () {
-      result.textContent = xhr.responseText;
-    }, false);
-    xhr.addEventListener('error', function () {
-      result.textContent = 'サーバーエラーが発生しました。';
-    }, false);
-    xhr.open('POST', 'create.php', true);
-    xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
-    xhr.send('value=' + encodeURIComponent(document.getElementById('btn').value));
-    //変数自体が存在するか確認
-    console.log(typeof value === 'undefined');
-  }, false);
+  }
 }, false);
