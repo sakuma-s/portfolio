9573895817c998423265537097ca7c6589b56dc8
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-13 07:22:06 +0900

    追加部分削除

--- create.php
+++ create.php
@@ -8,7 +8,6 @@ $_POST['message'] = filter_input(INPUT_POST, 'message');
 $_GET['id'] = filter_input(INPUT_GET, 'id');
 $id = $_GET['id'];
 $_GET['page_id'] = filter_input(INPUT_GET, 'page_id');
-print($_REQUEST['value']);
 //データ登録
 function createBoard($db, $board)
 {
