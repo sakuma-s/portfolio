cb839db4df0d40f2704a38639847969413ce2a4a
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-13 07:25:33 +0900

    サーバーから取得したデータの受け取り部分が抜けていた為追加

--- good_ajax.js
+++ good_ajax.js
@@ -5,15 +5,17 @@ document.addEventListener('DOMContentLoaded', function () {
     good[i].addEventListener('click', function () {
       var result = document.getElementById('result');
       var xhr = new XMLHttpRequest();
-      xhr.addEventListener('loadstart', function () {
-        result.textContent = '通信中...';
-      }, false);
-      xhr.addEventListener('load', function () {
-        result.textContent = xhr.responseText;
-      }, false);
-      xhr.addEventListener('error', function () {
-        result.textContent = 'サーバーエラーが発生しました。';
-      }, false);
+      xhr.onreadystatechange = function () {
+        xhr.addEventListener('loadstart', function () {
+          result.textContent = '通信中...';
+        }, false);
+        xhr.addEventListener('load', function () {
+          result.textContent = xhr.responseText;
+        }, false);
+        xhr.addEventListener('error', function () {
+          result.textContent = 'サーバーエラーが発生しました。';
+        }, false);
+      };
       xhr.open('POST', 'create.php', true);
       xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
       xhr.send('value=' + encodeURIComponent(document.getElementsByClassName('good').value));
