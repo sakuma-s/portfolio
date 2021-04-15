7815a8813c13f12f16205d75067ef6c4ace93457
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-15 15:50:27 +0900

    goodボタンのデータ登録

--- good_ajax.php
+++ good_ajax.php
@@ -1,2 +1,19 @@
 <?php
 require 'connect.php';
+sleep(3);
+print($_REQUEST['name']);
+//データ登録
+function goodButton($db, $board)
+{
+    $statement = $db->prepare('INSERT INTO good SET good=?');
+    $statement->execute(array($board['good']));
+}
+
+//データ受け取り
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $board = [
+        'good' => $_POST['good']
+    ];
+}
+
+//データ表示(index.phpに記入)
