af0666fdd00e6f98ec7d5396152fdf3af71017d5
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-16 11:10:57 +0900

    配列の内容等変更

--- good_ajax.php
+++ good_ajax.php
@@ -5,19 +5,18 @@ error_reporting(E_ALL);
 require 'connect.php';
 sleep(3);
 print($_REQUEST['name']);
-var_dump($_SERVER['REQUEST_METHOD']);
 print_r($_POST['name']);
 //データ登録
 function goodButton($db, $board)
 {
     $statement = $db->prepare('INSERT INTO good SET good_id=?');
-    $statement->execute(array($board['good']));
+    $statement->execute(array($board['name']));
 }
 
 //データ受け取り
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $board = [
-        'good' => $_POST['name']
+        'name' => $_POST['name']
     ];
 }
 
