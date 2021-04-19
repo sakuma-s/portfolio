69f3e72acc8243f8bc1b20d983d8f362a9c116e7
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-19 11:31:50 +0900

    SQLファイルの作成

---
+++ first-bulletin-board.sql
@@ -0,0 +1,15 @@
+CREATE TABLE posts (
+  id int NOT NULL AUTO_INCRIMENT,
+  nickname varchar(50) NOT NULL,
+  message varchar(140) NOT NULL,
+  reply_message varchar(150) NOT NULL,
+  created datetime NOT NULL,
+  PRIMARY KEY ('id')
+)
+
+CREATE TABLE good (
+  id int NOT NULL AUTO_INCRIMENT,
+  good_id int NOT NULL,
+  PRIMARY KEY ('id'),
+  CONSTRAINT posts_id FOREIGN KEY id ON DELETE CASCADE ON UPDATE CASCADE
+)
