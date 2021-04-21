3293f24c684e7e1c179bccfc2b8dd65cb9ebb9ca
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-21 15:09:29 +0900

    JSデータ送信確認2回目

--- good_ajax.php
+++ good_ajax.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 //sleep(3);
 //print($_REQUEST['name']);
 $_POST['name'] = filter_input(INPUT_POST, 'name');
-print('jsデータ送信確認' . $_POST['name']);
+print('jsデータ送信確認' . $_POST['name'] . 'です');
 //var_dump($good); //NULLと表示される。ボタンを押すとgoodが表示される。
 //データ登録
 function goodButton($db, $good)
