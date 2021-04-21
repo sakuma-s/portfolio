0478cd999a22f8c448f010660840bcc672d40d2f
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-21 16:23:43 +0900

    取得したデータを仮登録する

--- good_ajax.php
+++ good_ajax.php
@@ -2,12 +2,10 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-// require 'connect.php';
-//sleep(3);
-//print($_REQUEST['name']);
+require 'connect.php';
+$db = connect();
 $_POST['name'] = filter_input(INPUT_POST, 'name');
 print('jsデータ送信確認' . $_POST['name'] . 'です');
-// var_dump($_POST['name']);//表示確認OK
 $good = $_POST['name'];
 //データ登録
 function goodButton($db, $good)
@@ -15,13 +13,4 @@ function goodButton($db, $good)
     $statement = $db->prepare('INSERT INTO good SET good_id=?');
     $statement->execute(array($good));
 }
-
-//データ受け取り
-//今の所必要ない？
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//     $board = [
-//         'name' => $_POST['name']
-//     ];
-// }
-
-//データ表示(index.phpに記入)
+//取得しカウントした値を(index.phpに表示)
