0862fc576ed166f98854405616a5df3598b97e7d
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-21 15:23:11 +0900

    削除前に戻す

--- index.php
+++ index.php
@@ -63,6 +63,7 @@ list($page, $maxPage, $list) = pagiNation($db);
                             <div><?php echo $value['created']; ?></div>
                             <i class="far fa-thumbs-up"><input class="good" type="button" name="good" value="good">
                             </i>
+                            <div id="result"></div>
                         </div>
                     </div>
                 </div>
