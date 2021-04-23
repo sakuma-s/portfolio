3ccdd03d654b251c9d8704e6419f0ba85b2006b8
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-23 16:27:51 +0900

    変更を元に戻す

--- good_ajax.php
+++ good_ajax.php
@@ -3,12 +3,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $_POST['name'] = filter_input(INPUT_POST, 'name');
-// var_dump($good) . "goodの値";
-// echo $good . "goodの値";
+print($_POST['name']);
+var_dump($good) . "goodの値";
+echo $good . "goodの値";
 //データ登録
 function goodButton($db, $good)
 {
-    $good = $_POST['name'];
     $statement = $db->prepare('INSERT INTO good SET good_id=?');
     $statement->execute($good);
 }
