29f1c743bd5e2fe0574f1a4dcfdc512773576ef8
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-23 16:53:53 +0900

    SQLの実行文を変更

--- good_ajax.php
+++ good_ajax.php
@@ -4,12 +4,14 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $_POST['name'] = filter_input(INPUT_POST, 'name');
 print($_POST['name']);
+$good = $_POST['name'];
 var_dump($good) . "goodの値";
 echo $good . "goodの値";
 //データ登録
-function goodButton($db, $good)
+function goodButton($db)
 {
     $statement = $db->prepare('INSERT INTO good SET good_id=?');
-    $statement->execute($good);
+    $statement->bindValue(':good_id', $_POST['name']);
+    $statement->execute();
 }
 //取得しカウントした値を(index.phpに表示)
