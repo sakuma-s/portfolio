80ad69ddf24ce7b89df698b7f0a7cee24c15996b
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-23 16:23:46 +0900

    データ登録されないので調整

--- good_ajax.php
+++ good_ajax.php
@@ -3,14 +3,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $_POST['name'] = filter_input(INPUT_POST, 'name');
-print($_POST['name']);
-$good = $_POST['name'];
-var_dump($good) . "goodの値";
-echo $good . "goodの値";
+// var_dump($good) . "goodの値";
+// echo $good . "goodの値";
 //データ登録
 function goodButton($db, $good)
 {
+    $good = $_POST['name'];
     $statement = $db->prepare('INSERT INTO good SET good_id=?');
-    $statement->execute([$good]);
+    $statement->execute($good);
 }
 //取得しカウントした値を(index.phpに表示)
