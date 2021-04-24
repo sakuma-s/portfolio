362ebdabd8dc209677c5705944ac70c12825208b
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-24 15:10:38 +0900

    関数を削除[必要であれば再設定]

--- good_ajax.php
+++ good_ajax.php
@@ -5,20 +5,10 @@ error_reporting(E_ALL);
 require_once('connect.php');
 $db = connect();
 $good = filter_input(INPUT_POST, 'name');
-var_dump($good); //最初はNULL→ボタンを押すとgood
-// var_dump($_POST['name']); //NULL
-// var_dump($good['good']); //NULL
-echo ($good) . PHP_EOL;
-
-// echo $good . "goodの値"; //最初値がNULL。goodButtonを押すと、buttonの下に値が表示される。
+echo ($good); //最初はNULL→ボタンを押すとgood
 //データ登録
-goodButton($db, $good);
-
-function goodButton($db, $good)
-{
-    if (isset($good)) {
-        $statement = $db->prepare('INSERT INTO good SET good_id=?');
-        $statement->bindValue(1, $good);
-        $statement->execute();
-    }
+if (isset($good)) {
+    $statement = $db->prepare('INSERT INTO good SET good_id=?');
+    $statement->bindValue(1, $good);
+    $statement->execute();
 }
