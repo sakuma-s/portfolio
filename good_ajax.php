79c5371ef3278fdfaaf53a11ad3902227055c42e
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-24 10:32:49 +0900

    でをNULLにしていので一旦コメントアウト

--- good_ajax.php
+++ good_ajax.php
@@ -3,15 +3,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $_POST['name'] = filter_input(INPUT_POST, 'name');
-$good = $_POST['name'];
+// $good = $_POST['name'];
 // var_dump($good) . "goodの値";
-echo $good . "goodの値"; //最初値がNULL。goodButtonを押すと
+echo $good . "goodの値"; //最初値がNULL。goodButtonを押すと、buttonの下に値が表示される。
 //データ登録
 function goodButton($db, $good)
 {
     // $good = $_POST['name']; //取得できてい
     // echo $good . "goodの値" . "2"; //値が表示されない。↓NULLで登録されてしまう
-    if (!is_null($good['good'])) {
+    if (isset($good['good'])) {
         $statement = $db->prepare('INSERT INTO good SET good_id=?');
         $statement->bindValue(1, $good['good']);
         $statement->execute();
