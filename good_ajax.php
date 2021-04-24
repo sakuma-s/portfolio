2df8dc62f12906fc4e295d235eae09c9b6c1a70b
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-24 14:57:26 +0900

    データベース接続の追加を関数で試してみる

--- good_ajax.php
+++ good_ajax.php
@@ -5,7 +5,6 @@ error_reporting(E_ALL);
 require_once('connect.php');
 $db = connect();
 $good = filter_input(INPUT_POST, 'name');
-// $good = $_POST['name'];
 var_dump($good); //最初はNULL→ボタンを押すとgood
 // var_dump($_POST['name']); //NULL
 // var_dump($good['good']); //NULL
@@ -13,18 +12,13 @@ echo ($good) . PHP_EOL;
 
 // echo $good . "goodの値"; //最初値がNULL。goodButtonを押すと、buttonの下に値が表示される。
 //データ登録
-// function goodButton($db, $good)
-// {
-// $good = $_POST['name']; //取得できてい
-// echo $good . "goodの値" . "2"; //値が表示されない。↓NULLで登録されてしまう
-if (isset($good)) {
-    $statement = $db->prepare('INSERT INTO good SET good_id=?');
-    $statement->bindValue(1, $good);
-    $statement->execute();
+goodButton($db, $good);
+
+function goodButton($db, $good)
+{
+    if (isset($good)) {
+        $statement = $db->prepare('INSERT INTO good SET good_id=?');
+        $statement->bindValue(1, $good);
+        $statement->execute();
+    }
 }
-// }
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//     $good = [
-//         'good' => $_POST['name']
-//     ];
-// }
