7525985b7acb51480138bf5863ba15f517da843c
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-24 09:10:12 +0900

    現状の内容をコメントで追加

--- good_ajax.php
+++ good_ajax.php
@@ -3,15 +3,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $_POST['name'] = filter_input(INPUT_POST, 'name');
-print($_POST['name']);
 $good = $_POST['name'];
-var_dump($good) . "goodの値";
+// var_dump($good) . "goodの値";
 echo $good . "goodの値";
 //データ登録
 function goodButton($db, $good)
 {
     $good = $_POST['name'];
-    echo $good . "goodの値" . "2";
+    echo $good . "goodの値" . "2"; //値が表示されない
     $statement = $db->prepare('INSERT INTO good SET good_id=?');
     $statement->bindValue(1, $good);
     $statement->execute();
