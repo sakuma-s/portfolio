96df6909464852fa8a879d8f560b6d93fa5c1087
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-24 14:24:47 +0900

    ajaxの読み込みファイルをコメントアウト

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $errors = [];
 $board = [];
 require_once('connect.php');
 require_once('create.php');
-require_once('good_ajax.php');
+// require_once('good_ajax.php');
 $db = connect();
 createBoard($db, $board);
 deleteBoard($db, $posts_id);
