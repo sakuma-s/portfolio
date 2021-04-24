45a4d0fd9c0c6d233bbc9b05331e975a89594397
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-24 14:29:52 +0900

    データベース接続の追加

--- good_ajax.php
+++ good_ajax.php
@@ -2,6 +2,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+require_once('connect.php');
+$db = connect();
 $good = filter_input(INPUT_POST, 'name');
 // $good = $_POST['name'];
 var_dump($good); //最初はNULL→ボタンを押すとgood
