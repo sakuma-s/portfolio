6f27c98d6b401612dd2c90326b627afdb3ceab62
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-24 11:17:44 +0900

    の最初の値を確認

--- good_ajax.php
+++ good_ajax.php
@@ -4,9 +4,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $_POST['name'] = filter_input(INPUT_POST, 'name');
 // $good = $_POST['name'];
-// var_dump($good) . "goodの値";
+var_dump($_POST['name']);
 echo ($_POST['name']);
-echo $good . "goodの値"; //最初値がNULL。goodButtonを押すと、buttonの下に値が表示される。
+// echo $good . "goodの値"; //最初値がNULL。goodButtonを押すと、buttonの下に値が表示される。
 //データ登録
 function goodButton($db, $good)
 {
