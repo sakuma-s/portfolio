8c03230dcbc59cb83f23a1debf4c8d5a364d7cde
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-26 06:41:20 +0900

    ボタン表示部分を編集

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ list($page, $maxPage, $list) = pagiNation($db);
                             <div><?php echo $value['created']; ?></div>
                             <i class="far fa-thumbs-up"><input class="good" type="button" name="good" value="good">
                             </i>
-                            <div class="result" name="result"></div>
+                            <div class="result"></div>
                         </div>
                     </div>
                 </div>
