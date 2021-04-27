2a57350dafeeece105128c371092f399b976ed88
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-04-27 15:45:09 +0900

    faviconエラーの解消

--- index.php
+++ index.php
@@ -21,6 +21,8 @@ list($page, $maxPage, $list) = pagiNation($db);
     <meta name="viewport" content="width=device-width,initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link rel="icon" href="images/1535965055.png" type="images/x-icon" />
+    <link rel="shortcut icon" href="images/1535965055.png" type="images/x-icon" />
     <title>励まし会議</title>
 </head>
 
