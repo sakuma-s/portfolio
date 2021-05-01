b76163598b8f5c9535e5078362d4550310fc3546
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-01 11:13:56 +0900

    posts_idを受け取れているか確認

--- good_ajax.php
+++ good_ajax.php
@@ -6,11 +6,13 @@ require_once('connect.php');
 $db = connect();
 $good = filter_input(INPUT_POST, 'name');
 echo ($good); //最初はNULL→ボタンを押すとgood
+$posts_id = filter_input(INPUT_POST, 'data-posts_id');
+echo ($posts_id);
 sleep(2);
 echo var_export($good);
 //データ登録
 if (isset($good)) {
-    $statement = $db->prepare('INSERT INTO good SET good_id=?');
+    $statement = $db->prepare('INSERT INTO good SET good_id=? posts_id=?');
     $statement->bindValue(1, $good);
     $statement->execute();
 }
