4fce77e6cff0c1f536f397389a741bd403e3bc71
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-01 16:32:50 +0900

    SQL文変更

--- good_ajax.php
+++ good_ajax.php
@@ -6,15 +6,15 @@ require_once('connect.php');
 $db = connect();
 $good = filter_input(INPUT_POST, 'name');
 echo ($good); //最初はNULL→ボタンを押すとgood
-// $posts_id = filter_input(INPUT_POST, 'data-posts_id');
-// echo ($posts_id); //NULLになる
+$posts_id = filter_input(INPUT_POST, 'data-posts_id');
+echo ($posts_id); //NULLになる
 sleep(2);
-// var_dump($posts_id);
+var_dump($posts_id);
 echo var_export($good);
 //データ登録
-if (isset($good)) {
-    $statement = $db->prepare('INSERT INTO good SET good_id=?');
+if (isset($good, $posts_id)) {
+    $statement = $db->prepare('INSERT INTO good SET good_id=? posts_id=?');
     $statement->bindValue(1, $good);
-    //$statement->bindValue(2, $posts_id);
+    $statement->bindValue(2, $posts_id);
     $statement->execute();
 }
