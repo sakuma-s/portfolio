b14ee7ecc0a2ec933ab0f236e67e5f698783c989
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-04 10:16:47 +0900

    クリックイベントの変更

--- good_ajax.js
+++ good_ajax.js
@@ -5,26 +5,26 @@ document.addEventListener('DOMContentLoaded', function () {
     //全てのgoodボタンにajaxを適用させる
     good[i].onclick = function () {
       console.log("click", this);
-      var result = document.getElementById('result');
-      var xhr = new XMLHttpRequest();
-      // console.log(good);
-      xhr.onreadystatechange = function () {
-        xhr.addEventListener('loadstart', function () {
-          result.textContent = '通信中...';
-        }, false);
-        xhr.addEventListener('load', function () {
-          result.textContent = xhr.responseText;
-        }, false);
-        xhr.addEventListener('error', function () {
-          result.textContent = 'サーバーエラーが発生しました。';
-        }, false);
-      };
-      xhr.open('POST', 'good_ajax.php', true);
-      xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
-      xhr.send('name=' + encodeURIComponent(document.getElementsByClassName('good')[0].value));
-      //値自体が存在するか確認
-      console.log(typeof value === 'undefined');
-      //console.log(data);
-    }, false);
+    };
+    var result = document.getElementById('result');
+    var xhr = new XMLHttpRequest();
+    // console.log(good);
+    xhr.onreadystatechange = function () {
+      xhr.addEventListener('loadstart', function () {
+        result.textContent = '通信中...';
+      }, false);
+      xhr.addEventListener('load', function () {
+        result.textContent = xhr.responseText;
+      }, false);
+      xhr.addEventListener('error', function () {
+        result.textContent = 'サーバーエラーが発生しました。';
+      }, false);
+    };
+    xhr.open('POST', 'good_ajax.php', true);
+    xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+    xhr.send('name=' + encodeURIComponent(document.getElementsByClassName('good')[0].value));
+    //値自体が存在するか確認
+    console.log(typeof value === 'undefined');
+    //console.log(data);
   }
 }, false);
