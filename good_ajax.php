c66991a469fb91be3026b91ee372e0745595bb29
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-07 08:56:02 +0900

    データ取得の部分でNULLエラー。if文を追加

--- good_ajax.php
+++ good_ajax.php
@@ -17,8 +17,10 @@ if (isset($posts_id)) {
     $statement->execute();
 }
 //データ取得
-$query = "SELECT good_id FROM good WHERE posts_id = ?";
-$select = $db->prepare($query);
-$select->bindValue(1, $posts_id);
-$select->execute();
-$goodList = $select->fetchAll(PDO::FETCH_COLUMN);//指定の１つのカラムを取得
+if (isset($posts_id)) {
+    $query = "SELECT good_id FROM good WHERE posts_id = ?";
+    $select = $db->prepare($query);
+    $select->bindValue(1, $posts_id);
+    $select->execute();
+    $goodList = $select->fetchAll(PDO::FETCH_COLUMN); //指定の１つのカラムを取得
+}
