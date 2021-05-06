991efa40892069fb87e594d9da5a3c2bc6c1e00c
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-06 10:53:57 +0900

    INSERT文に変更

--- good_ajax.php
+++ good_ajax.php
@@ -7,12 +7,12 @@ $db = connect();
 $good = filter_input(INPUT_POST, 'name');
 $posts_id = filter_input(INPUT_POST, 'data-posts_id');
 sleep(2);
-var_dump($posts_id);
+var_dump($posts_id); //NULLになる
 var_export($good);
 //データ登録
 if (isset($posts_id)) {
-    $query = "UPDATE good SET good_id = good_id + 1 posts_id = :posts_id";
+    $query = "INSERT INTO good SET good_id = good_id + 1 posts_id = ?";
     $statement = $db->prepare($query);
-    $statement->bindParam(":posts_id", $posts_id);
+    $statement->bindValue(1, $posts_id);
     $statement->execute();
 }
