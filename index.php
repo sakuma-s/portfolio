76d5d5b0edc417d0862d0606af47bbfd17d90fe9
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-07 14:10:13 +0900

    関数の順番の注意点についてコメント追加

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $db = connect();
 createBoard($db, $board);
 deleteBoard($db, $posts_id);
 list($page, $maxPage, $list) = pagiNation($db);
-require_once('good_ajax.php');
+require_once('good_ajax.php'); //この順番をcreate.phpの下にすると削除機能が動作しなくなる
 // $goodList = goodCount($db);
 
 var_dump($goodList);
