59b2005b1ae95db5f29111a59ef759e4705f995b
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-07 09:42:02 +0900

    NULLになり接続が上手くできない

--- good_ajax.php
+++ good_ajax.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 // require_once('connect.php');
-// $db = connect();
+$db = connect();
 $good = filter_input(INPUT_POST, 'name');
 $posts_id = filter_input(INPUT_POST, 'data-posts_id');
 sleep(2);
@@ -12,7 +12,7 @@ var_export($good);
 //カウントアップ
 if (isset($posts_id)) {
     $query = "UPDATE good SET good_id = good_id + 1 WHERE posts_id = ?";
-    $statement = $db->prepare($query);
+    $statement = $db->prepare($query); //$dsnにする？
     $statement->bindValue(1, $posts_id);
     $statement->execute();
 }
