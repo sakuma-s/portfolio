7887cb4c20d3350d6db0f585ed7c363c0b0aa111
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-08 14:33:32 +0900

    同じ変数名に別の要素を入れてしまっていたので変更

--- good_ajax.php
+++ good_ajax.php
@@ -2,27 +2,25 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-// require_once('connect.php');
-// $db = connect();
 $good = filter_input(INPUT_POST, 'name');
-$posts_id = filter_input(INPUT_POST, 'data-posts_id');
+$dataPostId = filter_input(INPUT_POST, 'data-posts_id');
 sleep(2);
-var_dump($posts_id); //NULLになる
-var_export($good);
+// var_dump($dataPostId); //NULLになる
+// var_export($good);
 //カウントアップ
-if (isset($posts_id)) {
+if (isset($dataPostId)) {
     $query = "UPDATE good SET good_id = good_id + 1 WHERE posts_id = ?";
     $statement = $db->prepare($query); //$dsnにする？
-    $statement->bindValue(1, $posts_id);
+    $statement->bindValue(1, $dataPostId);
     $statement->execute();
 }
 //データ取得
 function goodCount($db)
 {
-    if (isset($posts_id)) {
+    if (isset($dataPostId)) {
         $query = "SELECT good_id FROM good WHERE posts_id = ?";
         $select = $db->prepare($query);
-        $select->bindValue(1, $posts_id);
+        $select->bindValue(1, $dataPostId);
         $select->execute();
         $goodList = $select->fetchAll(PDO::FETCH_ASSOC); //指定の１つのカラムを取得
     }
