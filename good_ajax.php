fda01e2d1a11c3d5253db1c8a2298eb32fd2f06b
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-11 10:55:44 +0900

    SQL文にposts_idを指定してデータ取得

--- good_ajax.php
+++ good_ajax.php
@@ -8,7 +8,7 @@ $goodList = filter_input(INPUT_GET, 'goodList');
 sleep(2);
 require_once('connect.php');
 $db = connect();
-var_dump($goodList);
+echo 'good_ajax.phpで指定' . var_dump($goodList);
 // var_dump($dataPostId); //NULLになる
 // var_export($good);
 //カウントアップ
@@ -21,14 +21,14 @@ if (isset($dataPostId)) {
 //データ取得
 // function goodCount($db)
 // {
-if (isset($dataPostId)) {
-    $query = "SELECT good_id FROM good WHERE posts_id = ?";
-    $select = $db->prepare($query);
-    $select->bindValue(1, $dataPostId);
-    $select->execute();
-    $goodList = $select->fetchAll(PDO::FETCH_ASSOC); //指定の１つのカラムを取得
+// if (isset($dataPostId)) {
+$query = "SELECT good_id FROM good WHERE posts_id = 784";
+$select = $db->prepare($query);
+// $select->bindValue(1, $dataPostId);
+$select->execute();
+$goodList = $select->fetchAll(PDO::FETCH_ASSOC); //指定の１つのカラムを取得
     // } else {
     //     echo "データを取得できませんでした";
-}
+// }
 // return $goodList;
 // }
