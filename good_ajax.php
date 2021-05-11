5f373b1450145a023d3e59a520e1a7097439d290
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-11 14:24:22 +0900

    SQL文の実行部分をif文の外で行う

--- good_ajax.php
+++ good_ajax.php
@@ -10,7 +10,7 @@ require_once('connect.php');
 $db = connect();
 // echo 'good_ajax.phpで指定' . var_dump($goodList); //NULL
 var_dump($dataPostId); //ボタンを押すとidが表示される
-var_dump($goodList); //NULLになる
+var_dump($goodList); //NULLになる。GETの時の場合。
 // var_export($good);
 //カウントアップ
 if (isset($dataPostId)) {
@@ -24,7 +24,7 @@ if (isset($dataPostId)) {
     $query = "SELECT good_id FROM good WHERE posts_id = ?";
     $select = $db->prepare($query);
     $select->bindValue(1, $dataPostId);
-    $select->execute();
-    $goodList = $select->fetchAll(PDO::FETCH_ASSOC);
-    var_dump($goodList);
 }
+$select->execute(); //上記のif文から抜ける
+$goodList = $select->fetchAll(PDO::FETCH_ASSOC);
+var_dump($goodList); //データ取得できていた
