b0580dcdc74f1fd8f78dc43b3130f99fcc88c129
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-14 10:46:37 +0900

    SQL文の編集

--- first-bulletin-board.sql
+++ first-bulletin-board.sql
@@ -1,16 +1,18 @@
 CREATE TABLE posts (
-  id int NOT NULL AUTO_INCRIMENT,
+  posts_id int NOT NULL AUTO_INCRIMENT,
   nickname varchar(50) NOT NULL,
   message varchar(140) NOT NULL,
   reply_message varchar(150) NOT NULL,
-  created_at datetime NOT NULL,
+  good_count INT NOT NULL,
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL,
   PRIMARY KEY ('id')
 )
 
-CREATE TABLE good (
-  id int(11) NOT NULL AUTO_INCRIMENT,
-  good_id int(11) NOT NULL,
-  posts_id int(11) NOT NULL,
-  PRIMARY KEY ('id'),
-  CONSTRAINT posts_id FOREIGN KEY posts_id REFERENCES posts posts_id ON DELETE CASCADE ON UPDATE CASCADE
-)
+-- CREATE TABLE good (
+--   id int(11) NOT NULL AUTO_INCRIMENT,
+--   good_id int(11) NOT NULL,
+--   posts_id int(11) NOT NULL,
+--   PRIMARY KEY ('id'),
+--   CONSTRAINT posts_id FOREIGN KEY posts_id REFERENCES posts posts_id ON DELETE CASCADE ON UPDATE CASCADE
+-- )
