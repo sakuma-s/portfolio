deebae7577b3bd28e661fc414f34b77becbca735
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-15 18:05:45 +0900

    goodListの前にarrayを追加

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ list($page, $maxPage, $list) = pagiNation($db);
                             <div><a href="reply_message.php?posts_id=<?php echo ($value['posts_id']); ?>">[コメント]</a><?php echo h($value['reply_message']); ?></div>
                             <div><a href="?posts_id=<?php echo ($value['posts_id']) ?>">[削除]</a></div>
                             <div><?php echo $value['created_at']; ?></div>
-                            <?php foreach ($goodList as $value) : ?>
+                            <?php foreach ((array)$goodList as $value) : ?>
                                 <i class="far fa-thumbs-up"><input data-posts_id=<?php echo ($value['posts_id']); ?> class="good" type="button" name="good" value="good">&nbsp;<?php echo ($value['good_count']); ?>
                                 </i>
                                 <div id="result"></div>
