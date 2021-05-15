956f5da2cb912e7ff6f0701d19a2afdbcf0d030e
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-15 17:58:07 +0900

    未定義のエラーを改善

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $errors = [];
 $board = [];
+$goodList = filter_input(INPUT_GET, '$goodList');
 require_once('connect.php');
 require_once('create.php');
 require_once('good_ajax.php');
