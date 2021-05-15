1dc87e077985f524a828a4e4bbac2de9f190a766
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-15 18:17:43 +0900

    データ取得についてコメント追加

--- good_ajax.php
+++ good_ajax.php
@@ -20,5 +20,5 @@ if (isset($dataPostId)) {
     $select = $db->prepare($query);
     $select->execute();
     $goodCount = $select->fetchAll(PDO::FETCH_ASSOC);
-    var_dump($goodCount);
+    // var_dump($goodCount);//一番上に配列でカウント数が表示される
 }
