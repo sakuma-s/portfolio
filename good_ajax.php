44df19a2460bdfe3b06b8f6a49dd5ee71f55911c
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-15 10:37:04 +0900

    goodボタンが押された時にメッセージを表示する

--- good_ajax.php
+++ good_ajax.php
@@ -4,11 +4,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $good = filter_input(INPUT_POST, 'name');
 $dataPostId = filter_input(INPUT_POST, 'data-posts_id'); //最初はNULL。ボタンを押すとid表示。
-$goodList = filter_input(INPUT_GET, 'goodList');
 require_once('connect.php');
 $db = connect();
-var_dump($dataPostId); //ボタンを押すとidが表示される.押す前はNULL。
-var_dump($goodList); //NULLになる。GETの時の場合。関数をなくしボタンを押すと配列が表示される
+echo ($_REQUEST($dataPostId) . "にgoodボタンが押されました"); //ボタンを押すとidが表示される.押す前はNULL。
 //カウントアップ
 if (isset($dataPostId)) {
     $query = "UPDATE posts SET good_count = good_count + 1 WHERE posts_id = ?";
