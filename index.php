5d6ff02906372592085cdfdefefff353e0d90013
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-17 16:08:44 +0900

    カウント数が表示されなくなってしまった

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ var_dump($goodCount);
                             <div><a href="reply_message.php?posts_id=<?php echo ($value['posts_id']); ?>">[コメント]</a><?php echo h($value['reply_message']); ?></div>
                             <div><a href="?posts_id=<?php echo ($value['posts_id']) ?>">[削除]</a></div>
                             <div><?php echo $value['created_at']; ?></div>
-                            <i class="far fa-thumbs-up"><input data-posts_id=<?php echo ($value['posts_id']); ?> class="good" type="button" name="good" value="good">&nbsp;<?php echo $_POST['good_count']; ?>
+                            <i class="far fa-thumbs-up"><input data-posts_id=<?php echo ($value['posts_id']); ?> class="good" type="button" name="good" value="good">&nbsp;<?php echo ($value['good_count']); ?>
 
                             </i>
                             <div id="result"></div>
