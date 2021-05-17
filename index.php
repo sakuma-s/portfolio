d474f572891688c09a35d1ce0a97d6f94a82ca14
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-17 11:21:50 +0900

    スペルミス修正

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ list($page, $maxPage, $list) = pagiNation($db);
             </div>
         </form>
         <main>
-            <?php foreach ($List as $value) : ?>
+            <?php foreach ($list as $value) : ?>
                 <div class="card">
                     <div class="card-body">
                         <div class="card-text">
