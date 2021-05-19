a62436f12fd87d16a03734bd51d67277842ab4cb
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-19 15:20:35 +0900

    data-post_idを受け取った時データ取得する(一番最初に戻す)

--- good_ajax.php
+++ good_ajax.php
@@ -2,17 +2,11 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-// $good = filter_input(INPUT_POST, 'name');
 $dataPostId = filter_input(INPUT_POST, 'data-posts_id'); //最初はNULL。ボタンを押すとid表示。
-// $goodCount = filter_input(INPUT_GET, 'goodCount');
 require_once('connect.php');
 $db = connect();
-var_dump($dataPostId);
-// $dataId = isset($dataPostId); //関数で使用する時にNULLの値を渡したくないので確認
-// var_dump($dataId);//ボタンを押すとtrue。最初はfalse。
-var_dump($goodCount);
-// var_dump($good);
-// print($_REQUEST['data-posts_id'] . 'にgoodボタンが押されました'); //idが表示される
+var_dump($dataPostId); //最初はNULL
+var_dump($goodCount); //最初はNULL
 //カウントアップ
 if (isset($dataPostId)) {
     $query = "UPDATE posts SET good_count = good_count + 1 WHERE posts_id = ?";
@@ -21,22 +15,22 @@ if (isset($dataPostId)) {
     $statement->execute();
 }
 //データ取得表示
-function goodList($db, $dataPostId)
-{
-    if (isset($dataPostId)) {
-        $goodCount = [];
-        $query = "SELECT good_count FROM posts";
-        $select = $db->prepare($query);
-        $select->execute();
-        $goodCount = $select->fetchAll(PDO::FETCH_ASSOC);
-        // var_dump($goodCount); //一番上に配列でカウント数が表示される
-        //echo gettype($goodCount); //array
-    }
-    return $goodCount;
+// function goodList($db, $dataPostId) //関数にするとNULLになる
+// {
+if (isset($dataPostId)) {
+    $goodCount = [];
+    $query = "SELECT good_count FROM posts";
+    $select = $db->prepare($query);
+    $select->execute();
+    $goodCount = $select->fetchAll(PDO::FETCH_ASSOC);
+    // var_dump($goodCount); //一番上に配列でカウント数が表示される
+    //echo gettype($goodCount); //array
 }
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $dataPostId = [
-        'data-posts_id' => $_POST['data-posts_id']
-    ];
-    $goodCount = goodList($db, $dataPostId);
-}
+// return $goodCount;
+// }
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//     $dataPostId = [
+//         'data-posts_id' => $_POST['data-posts_id']
+//     ];
+//     $goodCount = goodList($db, $dataPostId);
+// }
