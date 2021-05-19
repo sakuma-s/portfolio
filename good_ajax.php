c784bac3db69ff5d8ff0efafc47c33f98890b7dc
Sakuma Shun <star.of.factory.s.s@gmail.com>
2021-05-19 14:39:55 +0900

    postされた時に関数を実行する

--- good_ajax.php
+++ good_ajax.php
@@ -34,10 +34,9 @@ function goodList($db, $dataPostId)
     }
     return $goodCount;
 }
-$goodCount = goodList($db, $dataPostId);
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//     $dataPostId = [
-//         'data-posts_id' => $_POST['data-posts_id']
-//     ];
-//     $goodCount = goodList($db, $dataPostId);
-// }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // $dataPostId = [
+    //     'data-posts_id' => $_POST['data-posts_id']
+    // ];
+    $goodCount = goodList($db, $dataPostId);
+}
