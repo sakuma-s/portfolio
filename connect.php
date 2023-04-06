7737aef694111863d4f4f87b82257864c62da5cd
Sakuma Shun <star.of.factory.s.s@gmail.com>
2023-04-07 08:32:30 +0900

    ローカルで動作確認

--- connect.php
+++ connect.php
@@ -6,7 +6,7 @@ function connect()
     $dsn = "mysql:host={$db['%']};dbname={$db['dbname']};charset=utf8";
     $driver_options = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone='+09:00'"];
     try {
-        $db = new PDO($dsn, $db['saaaMPro.local'], $db['pass'], $driver_options);
+        $db = new PDO($dsn, $db['saaaMPro.local'], $db['55shiningstar'], $driver_options);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     } catch (PDOException $Exception) {
