485213da6b487d2fb929e8cc1c6b193df4ed3b1e
Sakuma Shun <star.of.factory.s.s@gmail.com>
2023-04-08 14:12:28 +0900

    ローカル環境で動かす

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 function connect()
 {
-    $db = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $db['dbname'] = ltrim($db['path'], '/'); //pathのみ取り出す
+    // $db = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    // $db['dbname'] = ltrim($db['path'], '/'); //pathのみ取り出す
     $dsn = "mysql:host={$db['root']};dbname={$db['dbname']};charset=utf8";
     $driver_options = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone='+09:00'"];
     try {
