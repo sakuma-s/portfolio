4959f61d1094225f189692c9b18b64bc3426cca5
Sakuma Shun <star.of.factory.s.s@gmail.com>
2023-04-08 14:05:37 +0900

    不正なアクセスが来たときに指定リンクへ飛ばす

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ $goodCount = goodList($db);
 <html lang="ja">
 
 <head>
+    <meta http-equiv="refresh" content="0; URL=https://www.google.com/?hl=ja">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
