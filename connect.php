602bc57ceee0cbd932a7ccce533604e9f5c08a2b
Sakuma Shun <star.of.factory.s.s@gmail.com>
2023-04-09 17:15:22 +0900

    データベース接続をローカル環境に変更

--- connect.php
+++ connect.php
@@ -3,10 +3,16 @@ function connect()
 {
     // $db = parse_url(getenv("CLEARDB_DATABASE_URL"));
     // $db['dbname'] = ltrim($db['path'], '/'); //pathのみ取り出す
-    $dsn = "mysql:host={$db['root']};dbname={$db['dbname']};charset=utf8";
-    $driver_options = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone='+09:00'"];
+
+    // $dsn = "mysql:dbname=;host=saaaMPro.localhost;port=3306;charset=utf8mb4";
+    // $driver_options = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone='+09:00'"];
+    $host = "localhost";
+    $dbname = "portlolio";
+    $user = "saaaMPro.local";
+    $password = "kirasan098";
+
     try {
-        $db = new PDO($dsn, $db['root'], $db['root'], $driver_options);
+        $db = new PDO("mysql:host=$host", "dbname=$dbname", $user, $password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     } catch (PDOException $Exception) {
